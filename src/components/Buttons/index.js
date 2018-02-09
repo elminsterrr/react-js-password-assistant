ef448e9a0a272d738f07796436ee79bc3bce85b8
elminsterrr <przemyslaw.glab@gmail.com>
2018-02-09 03:29:48 +0100

    Component refactor with map to produce 20 buttons

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 
 const style = {
@@ -9,45 +9,40 @@ const style = {
   },
 };
 
-class Buttons extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { password: '' };
-  }
+const Buttons = () => {
+  const arr = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+  ];
 
-  handleButtonSelect() {
-    console.log('test');
-  }
+  const allButtons = arr.map(el => (
+    <RaisedButton
+      key={el}
+      label={el}
+      style={style.button}
+      onClick={() => this.handleButtonSelect()}
+    />
+  ));
 
-  render() {
-    return (
-      <div>
-        <RaisedButton
-          label="1"
-          style={style.button}
-          onClick={() => this.handleButtonSelect()}
-        />
-        <RaisedButton label="2" style={style.button} />
-        <RaisedButton label="3" style={style.button} />
-        <RaisedButton label="4" style={style.button} />
-        <RaisedButton label="5" style={style.button} />
-        <RaisedButton label="6" style={style.button} />
-        <RaisedButton label="7" style={style.button} />
-        <RaisedButton label="8" style={style.button} />
-        <RaisedButton label="9" style={style.button} />
-        <RaisedButton label="10" style={style.button} />
-        <RaisedButton label="11" style={style.button} />
-        <RaisedButton label="12" style={style.button} />
-        <RaisedButton label="13" style={style.button} />
-        <RaisedButton label="14" style={style.button} />
-        <RaisedButton label="15" style={style.button} />
-        <RaisedButton label="16" style={style.button} />
-        <RaisedButton label="17" style={style.button} />
-        <RaisedButton label="18" style={style.button} />
-        <RaisedButton label="19" style={style.button} />
-        <RaisedButton label="20" style={style.button} />
-      </div>
-    );
-  }
-}
+  return <div>{allButtons}</div>;
+};
+
 export default Buttons;
